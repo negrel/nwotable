cc01bb093e24f275687d01e3662d028e79986aa8
nergel3 <alexnergel13@gmail.com>
2019-09-25 13:13:17 +0200

    script update

--- .eslintrc.js
+++ .eslintrc.js
@@ -59,7 +59,7 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'off',
     'no-tabs': 'off',
     // allow console.log during development only
-    // 'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     // allow debugger during development only
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }

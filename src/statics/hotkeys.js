df688e6236a674f5ec0f4a9ac6bedd0d3c7f1524
nergel3 <alexnergel13@gmail.com>
2019-09-30 21:14:43 +0200

    remove bug

--- src/statics/hotkeys.js
+++ src/statics/hotkeys.js
@@ -1,9 +1,7 @@
 window.addEventListener('keydown', (event) => {
-  event.preventDefault();
   console.log(event);
 
   if (event.key.match(/s|S/) && event.ctrlKey) {
     event.preventDefault();
-    console.log('CTRL + S');
   }
 });

3822f55916b9bfc7daeff52d83a2d24490329174
nergel3 <alexnergel13@gmail.com>
2019-09-30 21:24:49 +0200

    remove bug

--- src/statics/hotkeys.js
+++ src/statics/hotkeys.js
@@ -1,7 +1,11 @@
 window.addEventListener('keydown', (event) => {
   console.log(event);
 
-  if (event.key.match(/s|S/) && event.ctrlKey) {
+  if (event.key === 's' | event.key === 'S' && event.ctrlKey) {
     event.preventDefault();
+  }
+
+  if (event.key === 'Tab' && event.ctrlKey) {
+    console.log('Changing note');
   }
 });
